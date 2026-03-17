7b89d6d3f3465420406926bbe41a03c92ab88925
Deepak Togloor <deepaktogloor87@gmail.com>
2026-03-17 18:14:43 +0530

    opensource chat model hf file added

---
+++ LangChain_Demo/Components/Model/LanguageModel/Open_Source/Chat_Model/HF_Chat.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.2-1B-Instruct",
+    task="text-generation")
+
+model = ChatHuggingFace(llm=llm)
+response = model.invoke("What is the capital of France?")
+print(response)
