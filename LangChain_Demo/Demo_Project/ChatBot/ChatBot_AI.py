61c7db695586d04ecc5b4edda7298bf10b720877
Deepak Togloor <deepaktogloor87@gmail.com>
2026-03-18 17:11:05 +0530

    chat bot enhanced with messages

--- LangChain_Demo/Demo_Project/ChatBot/ChatBot_AI.py
+++ LangChain_Demo/Demo_Project/ChatBot/ChatBot_AI.py
@@ -1,5 +1,6 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
@@ -7,16 +8,18 @@ llm = HuggingFaceEndpoint(
     repo_id='meta-llama/Llama-3.2-1B-Instruct')
 model = ChatHuggingFace(llm=llm)
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful assistant."),
+]
 
 while True:
     usr_input = input("You: ")
-    chat_history.append(usr_input)
+    chat_history.append(HumanMessage(content=usr_input))
     if usr_input.lower() in ['exit', 'quit']:
         print("Exiting the chat. Goodbye!")
         break
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print(f"AI: {result.content}")
 
 print(chat_history)
