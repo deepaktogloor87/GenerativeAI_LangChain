198da805b4b524d7d1c29fee1386a70a57ad91ae
Deepak Togloor <deepaktogloor87@gmail.com>
2026-03-19 14:03:55 +0530

    add constraints using fields

--- LangChain_Demo/Components/StructuredOutput/pydantic_demo.py
+++ LangChain_Demo/Components/StructuredOutput/pydantic_demo.py
@@ -1,11 +1,13 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 
 class Student(BaseModel):
     name: str = 'John Doe'
     age : Optional[int] = None
     email : EmailStr
+    cgpa : float = Field(..., gt=0.0, lt=10.0)
 
-new_student = {'age':35, 'email':'deepaktogloor@gmail.com'}
+
+new_student = {'age':35, 'email':'deepaktogloor@gmail.com', 'cgpa': 8.5 }
 student = Student(**new_student)
 print(student)
