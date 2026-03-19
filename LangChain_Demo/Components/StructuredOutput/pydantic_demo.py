b5327e88fe65d08e6de80032a38f9adb39893151
Deepak Togloor <deepaktogloor87@gmail.com>
2026-03-19 14:01:25 +0530

    email validation assertion done using pydantic

--- LangChain_Demo/Components/StructuredOutput/pydantic_demo.py
+++ LangChain_Demo/Components/StructuredOutput/pydantic_demo.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 class Student(BaseModel):
     name: str = 'John Doe'
     age : Optional[int] = None
+    email : EmailStr
 
-new_student = {'age':35}
+new_student = {'age':35, 'email':'deepaktogloor@gmail.com'}
 student = Student(**new_student)
 print(student)
