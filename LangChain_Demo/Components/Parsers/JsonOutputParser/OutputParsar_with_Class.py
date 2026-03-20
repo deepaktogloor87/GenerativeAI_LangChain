660196c4e1885dbb0c8e0f1aa901fd9b6048b3dc
Deepak Togloor <deepaktogloor87@gmail.com>
2026-03-20 13:16:34 +0530

    json parser with chain

--- LangChain_Demo/Components/Parsers/JsonOutputParser/OutputParsar_with_Class.py
+++ LangChain_Demo/Components/Parsers/JsonOutputParser/OutputParsar_with_Class.py
@@ -6,19 +6,22 @@ from langchain_core.output_parsers import JsonOutputParser
 from dotenv import load_dotenv
 load_dotenv()
 
-llm = HuggingFaceEndpoint(repo_id='zai-org/GLM-5')
+llm = HuggingFaceEndpoint(repo_id='meta-llama/Llama-3.1-8B-Instruct')
 
 model = ChatHuggingFace(llm=llm)
 
 parser = JsonOutputParser()
 
 template = PromptTemplate(
-    template = "Give me the name, age and city of a fictional person \n {format_instruction}",
-    input_variables=[], 
+    template = "Give me 5 important qualities of the {topic} \n {format_instruction}",
+    input_variables=["topic"], 
     partial_variables={'format_instruction':parser.get_format_instructions()}
 )
 
-prompt = template.format()
-result = model.invoke(prompt)
-final_result = parser.parse(result.content)
-print(final_result)
+chain = template | model | parser
+result = chain.invoke({'topic':'black pather'})
+print(result)
+# prompt = template.format()
+# result = model.invoke(prompt)
+# final_result = parser.parse(result.content)
+# print(final_result)
