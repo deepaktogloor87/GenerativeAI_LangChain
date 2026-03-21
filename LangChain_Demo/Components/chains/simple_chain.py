79cf6a5c8ed198f530528da5525942f3c6ee1fe1
Deepak Togloor <deepaktogloor87@gmail.com>
2026-03-21 21:25:36 +0530

    simple chain

---
+++ LangChain_Demo/Components/chains/simple_chain.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# HuggingFace endpoint LLM
+llm = HuggingFaceEndpoint(
+    repo_id='meta-llama/Llama-3.1-8B-Instruct',
+    task='text-generation'
+)
+
+# Chat model wrapper
+model = ChatHuggingFace(llm=llm)
+
+# Prompt
+prompt = ChatPromptTemplate(
+    template="Generate 5 facts about {topic}",
+    input_variables=["topic"]
+)
+
+# Output parser
+parser = StrOutputParser()
+
+# Chain using pipe syntax (works in langchain_core)
+chain = prompt | model | parser
+
+# Run chain
+result = chain.invoke({'topic': 'women'})
+print(result)
