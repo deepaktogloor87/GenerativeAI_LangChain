f5a82c0e9b40a52b839ac668fe7191b579ceaee0
Deepak Togloor <deepaktogloor87@gmail.com>
2026-03-29 16:09:02 +0530

    tool execution step added

--- LangChain_Demo/Components/Tools/ToolCalling/ToolsBinding.py
+++ LangChain_Demo/Components/Tools/ToolCalling/ToolsBinding.py
@@ -12,11 +12,11 @@ load_dotenv()
 def multiply(a: int, b: int) -> int:
     """Given 2 numbers a and b, return their product."""
     return a * b
-print(multiply.invoke({'a':3, 'b':4}))  # Output: 12
-print(multiply.description)  # Output: "Given 2 numbers a and b, return their product."
-print(multiply.args_schema)  # Output: {'a': <class 'int'>, 'b': <class 'int'>}
-print(multiply.name)  # Output: "multiply"
-print(multiply.args) # Output: {'a': 3, 'b': 4}
+# print(multiply.invoke({'a':3, 'b':4}))  # Output: 12
+# print(multiply.description)  # Output: "Given 2 numbers a and b, return their product."
+# print(multiply.args_schema)  # Output: {'a': <class 'int'>, 'b': <class 'int'>}
+# print(multiply.name)  # Output: "multiply"
+# print(multiply.args) # Output: {'a': 3, 'b': 4}
       
 # Tool binding
 def BoundedTool():
@@ -27,4 +27,9 @@ def BoundedTool():
 # Tool calling
 llm_with_tools = BoundedTool()
 response = llm_with_tools.invoke("can you multiply 5 and 6?")
-print(response.tool_calls[0])  # Output: "The product of 5 and 6 is 30."
+# print(response)  # Output: "The product of 5 and 6 is 30."
+# print(response.tool_calls[0])  # Output: "The product of 5 and 6 is 30."
+
+# Tools execution
+multiply_response = multiply.invoke(response.tool_calls[0])
+print(multiply_response)  # Output: 30
